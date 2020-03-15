0e8d1c9ba39617097d82bdbacbb04d40c1c6d1e4
syoukera <s.akira2986@gmail.com>
2020-03-15 13:51:57 +0900

    Update demo_3k.py: resume hard code

--- demo_3k.py
+++ demo_3k.py
@@ -85,8 +85,7 @@ def main():
     max_data_shape = [[('data', (1, 3, max([v[0] for v in config.TEST.SCALES]), max([v[1] for v in config.TEST.SCALES])))]]
     provide_data = [[(k, v.shape) for k, v in zip(data_names, data[i])] for i in xrange(len(data))]
     provide_label = [None for i in xrange(len(data))]
-    output_path = './output/chips_resnet101_3k/res101_mx_3k/aug_img'
-    model_prefix = os.path.join(output_path, 'CRCNN')
+    model_prefix = os.path.join('./output/chips_resnet101_3k/res101_mx_3k/', config.dataset.image_set, 'CRCNN')
     arg_params, aux_params = load_param(model_prefix, config.TEST.TEST_EPOCH,
                                         convert=True, process=True)
     # set model
@@ -94,8 +93,9 @@ def main():
     mod.bind(provide_data, provide_label, for_training=False)
     mod.init_params(arg_params=arg_params, aux_params=aux_params)   
 
-    # load the [index] - [class name] matching file
-    with open("./data/data_20200122_4class/list_classes.pickle",'rb') as f:
+    # load the [index] - [class name] matching fil
+    path = os.path.join(config.dataset.dataset_path, 'list_classes.pickle')
+    with open(path,'rb') as f:
         index2words = pickle.load(f)
     # index2words.insert(0, u'background')
      # index2words.pop(0)
