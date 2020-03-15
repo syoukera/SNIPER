fcccc9aed3108e16b3090df4fb9d4d721cfca8d2
syoukera <s.akira2986@gmail.com>
2020-03-15 13:55:11 +0900

    Update note_on_docker.sh

--- scripts/note_on_docker.sh
+++ scripts/note_on_docker.sh
@@ -6,11 +6,13 @@
 repository_dir="$(pwd)"
 data_dir="$(pwd)/../data"
 image_dir="/data_server_storage/clients/cvlab/kc-augmentation-tool/20200304_485classes_100k/regi_legacy"
+note_dir="$(pwd)/../note"
 
 # run
 docker run -it --rm \
 --user 1043:1043 \
 --runtime=nvidia \
+-p 49194:8888 \
 -v /etc/group:/etc/group:ro \
 -v /etc/passwd:/etc/passwd:ro \
 -v /etc/shadow:/etc/shadow:ro \
@@ -20,6 +22,7 @@ docker run -it --rm \
 -v $repository_dir:/working/rfcn3k \
 -v $data_dir:/working/rfcn3k/data \
 -v $image_dir:/working/rfcn3k/data/20200304_485classes_100k/regi_legacy \
--w /working/rfcn3k \
-syoukera/rfcn3k:multi-gpu \
+-v $note_dir:/working/note \
+-w /working \
+syoukera/rfcn3k:multi-gpu_note \
 jupyter notebook
